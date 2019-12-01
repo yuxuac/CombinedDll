522192c285f723593e4bd4454c0ca1a034336d6d
yuxuac <cui_yuxuan@126.com>
2019-12-01 16:45:23 +1100

    Create Project.

---
+++ CombineDlls/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using ThirdPartyTool;
+
+namespace CombineDlls
+{
+    class Program
+    {
+        static Program()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        static void Main(string[] args)
+        {
+            Util.SayHelloTo("Peter");
+            Console.ReadLine();
+        }
+
+        private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var requiredDllName = $"{(new AssemblyName(args.Name).Name)}.dll";
+            var resource = currentAssembly.GetManifestResourceNames().Where(s => s.EndsWith(requiredDllName)).FirstOrDefault();
+
+            if (resource != null)
+            {
+                using (var stream = currentAssembly.GetManifestResourceStream(resource))
+                {
+                    if (stream == null)
+                        return null;
+
+                    var block = new byte[stream.Length];
+                    stream.Read(block, 0, block.Length);
+                    return Assembly.Load(block);
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ThirdPartyTool/Util.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThirdPartyTool
+{
+    public class Util
+    {
+        public static void SayHelloTo(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
